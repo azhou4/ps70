ab6dab772eb134c46ac1ed86a295dfc046dd9b1c
Amy Zhou <amy_zhou@college.harvard.edu>
2021-04-13 11:51:10 -0400

    add gravity/friction

--- week11-draw/sketch.js
+++ week11-draw/sketch.js
@@ -1,5 +1,10 @@
 let path = [];
 let numPoints = 100;
+let numBalls = 13;
+let spring = 0.05;
+let gravity = 0.03;
+let friction = -0.9;
+let balls = [];
 
 function setup() {
 	createCanvas(710, 400, WEBGL);
@@ -9,6 +14,17 @@ function setup() {
     for (let i = 0; i < numPoints; i++ ) {
     	path[i] = new p5.Vector(0, 0, 0);
     }
+	for (let i = 0; i < numBalls; i++) {
+		balls[i] = new Ball(
+		  random(width),
+		  random(height),
+		  random(30, 70),
+		  i,
+		  balls
+		);
+	  }
+	  noStroke();
+	  fill(255, 204);
 
 }
 
@@ -19,6 +35,12 @@ function draw() {
 	drawGrid();
 	drawAxis();
 	drawPath(); 
+
+	balls.forEach(ball => {
+		ball.collide();
+		ball.move();
+		ball.display();
+	  });
 }
 
 
@@ -70,6 +92,7 @@ function drawPath() {
 		let val = i / numPoints * 204.0 + 51;
     	stroke(val);
 		line(path[i].x, path[i].y, path[i].z, path[i+1].x, path[i+1].y, path[i+1].z);
+		//circle(path[i].x, path[i].y, 5);
 	}
 
 	// Draw points
@@ -79,6 +102,66 @@ function drawPath() {
 	// })
 }
 
+class Ball {
+	constructor(xin, yin, din, idin, oin) {
+	  this.x = xin;
+	  this.y = yin;
+	  this.vx = 0;
+	  this.vy = 0;
+	  this.diameter = din;
+	  this.id = idin;
+	  this.others = oin;
+	}
+  
+	collide() {
+	  for (let i = this.id + 1; i < numBalls; i++) {
+		// console.log(others[i]);
+		let dx = this.others[i].x - this.x;
+		let dy = this.others[i].y - this.y;
+		let distance = sqrt(dx * dx + dy * dy);
+		let minDist = this.others[i].diameter / 2 + this.diameter / 2;
+		//   console.log(distance);
+		//console.log(minDist);
+		if (distance < minDist) {
+		  //console.log("2");
+		  let angle = atan2(dy, dx);
+		  let targetX = this.x + cos(angle) * minDist;
+		  let targetY = this.y + sin(angle) * minDist;
+		  let ax = (targetX - this.others[i].x) * spring;
+		  let ay = (targetY - this.others[i].y) * spring;
+		  this.vx -= ax;
+		  this.vy -= ay;
+		  this.others[i].vx += ax;
+		  this.others[i].vy += ay;
+		}
+	  }
+	}
+  
+	move() {
+	  this.vy += gravity;
+	  this.x += this.vx;
+	  this.y += this.vy;
+	  if (this.x + this.diameter / 2 > width) {
+		this.x = width - this.diameter / 2;
+		this.vx *= friction;
+	  } else if (this.x - this.diameter / 2 < 0) {
+		this.x = this.diameter / 2;
+		this.vx *= friction;
+	  }
+	  if (this.y + this.diameter / 2 > height) {
+		this.y = height - this.diameter / 2;
+		this.vy *= friction;
+	  } else if (this.y - this.diameter / 2 < 0) {
+		this.y = this.diameter / 2;
+		this.vy *= friction;
+	  }
+	}
+  
+	display() {
+	  ellipse(this.x, this.y, this.diameter, this.diameter);
+	}
+  }
+  
 
 
 (function() {
